c51a4aac087b67919ba5f33944c30b792ded9e72
ShrookZein <sroukzain7007@gmail.com>
2023-12-13 18:17:31 +0200

    Create Backend

---
+++ Back-end/src/main/java/com/spring/social/SocialMediaFeedsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaFeedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaFeedsApplication.class, args);
+	}
+
+}
